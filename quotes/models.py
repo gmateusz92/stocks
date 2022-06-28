6919842dfe3775a0f2d861ee8030611fe501e6ea
mateusz <matek1600@o2.pl>
2022-06-28 14:13:32 +0200

    1

--- quotes/models.py
+++ quotes/models.py
@@ -1,2 +1,9 @@
-from django.shortcuts import render
+
+from django.db import models
+
+class Stock(models.Model):
+    ticker = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.ticker
 
--- quotes/views.py
+++ quotes/views.py
@@ -1,17 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
+
 
 def home(request):
     import requests
     import json
 
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_297824bdbb614725932cf143763397a7")
+    if request.method == 'POST':
+        ticker = request.POST['ticker_symbol']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_297824bdbb614725932cf143763397a7")
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': 'Enter a ticker symbol above'})
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
 
-    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})
+
+def add_stock(request):
+    import requests
+    import json
+
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Stock added'))
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+        output = [] # dodjamy liste zeby zapisac bo jak bez listy to bedzie zapetlac i dawac inny obiekt
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_297824bdbb614725932cf143763397a7")
+
+            try:
+                api = json.loads(api_request.content)
+                output.append(api) # dodawanie do listy
+            except Exception as e:
+                api = "Error..."
+
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("stock  " + str(item) + "has been deleted"))
+    return redirect(add_stock)
